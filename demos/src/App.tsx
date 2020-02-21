7f9fef7bd9b414cb7866635c05bc1e04ff4f137a
ChrisDobby <dobby@chrisjdobson.co.uk>
2020-02-21 18:30:42 +0000

    initial project setup

---
+++ demos/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Test } from "react-webxr";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Test />
+        </div>
+    );
+};
+
+export default App;
