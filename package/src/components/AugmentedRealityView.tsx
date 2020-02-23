c42524e893bebc6d2e98f41f496ffa79795b6bc6
ChrisDobby <dobby@chrisjdobson.co.uk>
2020-02-23 18:16:46 +0000

    Styled the start button

--- package/src/components/AugmentedRealityView.tsx
+++ package/src/components/AugmentedRealityView.tsx
@@ -26,7 +26,12 @@ type AugmentedRealityViewProps = StandardProps & {
 type UnsupportedMessageProps = StandardProps & UnsupportedReasonProps;
 
 const StartStopButton = ({ onStartSelected }: StartComponentProps) => (
-    <button onClick={onStartSelected}>Start AR Session</button>
+    <button style={{ backgroundColor: "transparent", outline: 0, border: "none" }} onClick={onStartSelected}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+            <path d="M608 64H32C14.33 64 0 78.33 0 96v320c0 17.67 14.33 32 32 32h160.22c25.19 0 48.03-14.77 58.36-37.74l27.74-61.64C286.21 331.08 302.35 320 320 320s33.79 11.08 41.68 28.62l27.74 61.64C399.75 433.23 422.6 448 447.78 448H608c17.67 0 32-14.33 32-32V96c0-17.67-14.33-32-32-32zM160 304c-35.35 0-64-28.65-64-64s28.65-64 64-64 64 28.65 64 64-28.65 64-64 64zm320 0c-35.35 0-64-28.65-64-64s28.65-64 64-64 64 28.65 64 64-28.65 64-64 64z" />
+        </svg>{" "}
+        <span style={{ fontWeight: 700 }}>Start AR Session</span>
+    </button>
 );
 
 const UnsupportedMessage = (props: UnsupportedMessageProps) => {
